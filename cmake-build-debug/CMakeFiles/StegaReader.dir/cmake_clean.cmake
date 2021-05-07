aee8c1fca0173013ba40e4418ccada656e85bee2
Kabir Madan <kabirbmadan@gmail.com>
2021-05-08 00:34:09 +0800

    cmake-build

---
+++ cmake-build-debug/CMakeFiles/StegaReader.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StegaReader.dir/src/conversion.cpp.o"
+  "CMakeFiles/StegaReader.dir/src/io_util.cpp.o"
+  "CMakeFiles/StegaReader.dir/src/main.cpp.o"
+  "StegaReader"
+  "StegaReader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StegaReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
